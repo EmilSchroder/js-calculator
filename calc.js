856ecc63113f0a282c3cbe74b142050b46996b18
EmilSchroder <eschro66@gmail.com>
2018-07-12 11:31:45 +1200

    defined +,-,*,/ and they work

--- calc.js
+++ calc.js
@@ -8,21 +8,32 @@ function Clickon(idButt, inTag){
     
 
 
-    if (!isNaN(idButt) || idButt==='.'){
+    if (!isNaN(idButt)){
         temp += idButt;
-        output = temp;
-        document.getElementsByTagName(inTag)[0].value=output.substring(0,7);
+        document.getElementsByTagName(inTag)[0].value=temp.substring(0,7);
+    }
+    else if (idButt==='.' && temp.includes('.')===false){
+        temp += idButt;
+        document.getElementsByTagName(inTag)[0].value=temp.substring(0,7);
     }
     else if (idButt==='AC'){
-        temp="";
+        temp='';
         arr=[];
         output=0;
         document.getElementsByTagName(inTag)[0].value="";
     }
     else if (idButt==='CE'){
+        temp ='';
         document.getElementsByTagName(inTag)[0].value="";
     }
-    
+    else if (idButt==='+'||idButt==='-'||idButt==='*'||idButt==='/'){
+        arr.push(temp);
+        arr.push(idButt);
+        temp = '';
+    }
+    else if (idButt==='='){
+        
+    }
 
 
   
