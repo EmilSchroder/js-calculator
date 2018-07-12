d8fd7c495ed35d77e3ff9d7a17620104529eed19
EmilSchroder <eschro66@gmail.com>
2018-07-12 14:15:00 +1200

    broke into smaller functions

--- calc.js
+++ calc.js
@@ -2,50 +2,98 @@
 var temp="";
 var arr=[];
 var output=0;
+var s=''
+
+
 
 function Clickon(idButt, inTag){
-    
-    
 
-    //If number
+
     if (!isNaN(idButt)){
-        temp += idButt;
-        document.getElementsByTagName(inTag)[0].value=temp.substring(0,9);
-    }//If decimal point then don't allow twice
+        isNumber(idButt);
+    }
     else if (idButt==='.' && temp.includes('.')===false){
-        temp += idButt;
-        document.getElementsByTagName(inTag)[0].value=temp.substring(0,9);
-    }//AC clears everything
+        isDecimal(idButt);
+    }
     else if (idButt==='AC'){
+        isAC();
+    }
+    else if (idButt==='CE'){
+        isCE();
+    }
+    else if (idButt==='+'||idButt==='-'||idButt==='*'||idButt==='/'){
+        isOperator(idButt);
+    }
+    else if(idButt==='?'){
+        isRandom();
+    }
+    else if (idButt==='='){
+        isEquals(idButt);
+    }
+
+    document.getElementsByTagName(inTag)[0].value = s;
+}
+
+
+    /////////////////////////Subfunctions///////////////////////////
+    //If number
+    function isNumber(idButt){
+        temp += idButt;
+        s=temp.substring(0,9);
+    }
+    
+    //If decimal point then don't allow twice
+    function isDecimal(idButt){
+        temp += idButt;
+        s.substring(0,9);
+    }
+
+    //AC clears everything
+    function isAC(){
         temp='';
         arr=[];
         output=0;
-        document.getElementsByTagName(inTag)[0].value="";
-    }//CE clears visual and temp
-    else if (idButt==='CE'){
+        s="";
+    }
+
+    //CE clears visual and temp
+    function isCE(){
         temp ='';
-        document.getElementsByTagName(inTag)[0].value="";
-    }//Operators
-    else if (idButt==='+'||idButt==='-'||idButt==='*'||idButt==='/'){
+        s="";
+    }
+
+    //Operators
+    function isOperator(idButt){
         arr.push(temp);
         arr.push(idButt);
         temp = '';
     }
+
     //Who knows? It's a mystery!
-    else if(idButt==='?'){
+    function isRandom(){
         let a = String(Math.floor(Math.random()*100));
 
         if(a === 67){
-            document.getElementsByTagName(inTag)[0].value = "# MYSTERY";
+            s = "# MYSTERY";
         }else{        
-            document.getElementsByTagName(inTag)[0].value = a;
+            s = a;
         }temp += a;
-        
     }
+
     //Equals
-    else if (idButt==='='){
+    function isEquals(idButt){
         arr.push(temp);
-        //Start looping through all elements to perform operations
+
+        PerformCalc();
+        //getting the visuals right for the negative outputs
+        correctNegaSign();
+
+        temp=''
+        s = output;
+    }
+    
+    //Start looping through all elements to perform operations
+    function PerformCalc(){
         output = Number(arr[0]);
         for (let i=1; i<arr.length; i++){
             let plusOne = arr[i];
@@ -68,17 +116,12 @@ function Clickon(idButt, inTag){
                     break;
             }
         } 
-
         arr=[];
         arr.push(output);
-        //getting the visuals right for the negative outputs
+    }
+
+    function correctNegaSign(){
         if (output < 0) {
             output = Math.abs(output) + '-';
           } 
-        temp=''
-        document.getElementsByTagName(inTag)[0].value = output;
-    }
-
-
-  
-}
+        }
